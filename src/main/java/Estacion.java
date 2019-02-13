930cbb5e7ed7c6a1dd143eecf2b1946234401c8a
unknown <patezno97@gmail.com>
2019-02-13 11:40:22 +0100

    started estacion

--- src/main/java/Estacion.java
+++ src/main/java/Estacion.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Estacion {
+
+    // Atributos
+
+    private int id = 0;
+    private String direccion = "";
+    private int numeroAnclajes = 0;
+    private ArrayList<Integer> anclajes = new ArrayList<Integer>();
+
+    // Constructor
+
+    public Estacion(int id, String direccion, int numeroAnclajes) {
+        this.id = id;
+        this.direccion = direccion;
+        this.numeroAnclajes = numeroAnclajes;
+    }
+
+    // Metodos
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public int getNumeroAnclajes() {
+        return this.numeroAnclajes;
+    }
+
 }
--- src/test/java/BiciPalmaTest.java
+++ src/test/java/BiciPalmaTest.java
@@ -1,7 +1,23 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BiciPalmaTest {
 
+    private Estacion estacion = null;
+
+    @Before
+    public void init() {
+
+        estacion = new Estacion(1, "Manacor", 8);
+
+    }
+
     @Test
-    public void test() {}
+    public void gettersEstacionTest() {
+        assertEquals(1, estacion.getId());
+        assertEquals("Manacor", estacion.getDireccion());
+        assertEquals(8, estacion.getNumeroAnclajes());
+    }
 }
